dc3a39d9d0987f46ed01d652ab9906b4e32abe43
TolaAbiodun <adetolaabiodun@outlook.com>
2021-10-30 00:37:58 +0100

    added functional search bar

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,48 @@
+import React , {useState, useEffect, useRef} from 'react';
+
+// Import styles
+import {Wrapper, Content} from './SearchBar.styles';
+
+// Import Search Icon
+import searchIcon from '../../images/search-icon.svg';
+
+// Searbar component
+
+const SearchBar = ({ setSearchTerm }) => {
+    const [state, setState] = useState('');
+    const initial = useRef(true);
+
+    // Skip initial render in use effect
+    useEffect(() => {
+      if (initial.current) {
+        initial.current = false;
+        return;
+      }
+
+      // Create slight delay to fetch search term
+      const timer = setTimeout(() => {
+        setSearchTerm(state);
+      }, 600);
+
+      // Clear timer
+      return () => clearTimeout(timer);
+    }, [setSearchTerm, state]);
+
+
+    return (
+        <Wrapper>
+            <Content>
+                <img src={searchIcon} alt='search-icon' />
+                <input type='text' 
+                       placeholder='Search' 
+                       onChange = {e => setState(e.currentTarget.value)}
+                       value = {state}
+                />
+            </Content>
+        </Wrapper>
+    );
+};
+
+export default SearchBar;
+ 
+
