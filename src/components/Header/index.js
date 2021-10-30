b0435950ff2b7444535feb19089a96bb31270f1d
TolaAbiodun <adetolaabiodun@outlook.com>
2021-10-30 01:05:02 +0100

    created routes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -7,12 +8,14 @@ import {Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles';
 
 
 const Header = () => (
-    <Wrapper>
-        <Content>
-            <LogoImg src={RMDBLogo} alt='app-logo' />
-            <TMDBLogoImg src={TMDBLogo} alt='the movie database logo' />
-        </Content>
-    </Wrapper>
+  <Wrapper>
+    <Content>
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="app-logo" />
+      </Link>
+      <TMDBLogoImg src={TMDBLogo} alt="the movie database logo" />
+    </Content>
+  </Wrapper>
 );
 
 export default Header;
--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 // Import styles
 import { ThumbNails } from './Thumbnails.styles';
 
-const ThumbImg = ({image, movieId, clickable}) => (
-    <div>
-        <ThumbNails src={image} alt='movie-thumb-image'/>
-    </div>
+const ThumbImg = ({ image, movieId, clickable }) => (
+  <div>
+    {clickable ? (
+      <Link to={`/&{movieId}`}>
+        <ThumbNails src={image} alt="movie-thumb-image" />
+      </Link>
+    ) : (
+      <ThumbNails src={image} alt="movie-thumb-image" />
+    )}
+  </div>
 );
 
 export default ThumbImg;
