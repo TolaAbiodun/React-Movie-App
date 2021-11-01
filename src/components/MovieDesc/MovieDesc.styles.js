53a71c844ae287d8d176d2539d718e6bcb457406
TolaAbiodun <adetolaabiodun@outlook.com>
2021-11-01 12:38:28 +0100

    movie info

---
+++ src/components/MovieDesc/MovieDesc.styles.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+// Import config
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config';
+
+export const Wrapper = styled.div`
+    background: ${({ backdrop }) => 
+        backdrop ? `url(${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop})` : '#000'};
+    background-size: cover;
+    background-position: center;
+    padding: 40px 20px;
+    animation: animateMovieDesc 1s;
+
+    @keyframes animateMovieDesc {
+        from {
+            opacity:0;
+        }
+        to {
+            opacity:1;
+        }
+    }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    max-width: var(--maxWidth);
+    height: auto;
+    background: rgba(0, 0, 0, 0.7);
+    margin: 0 auto;
+    border-radius: 20px;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        max-height: none;
+
+    }
+`;
+
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  overflow: hidden;
+  color: var(--white);
+  text-align: justify;
+
+  @media (max-width: 768px) {
+    text-align: justify;
+    text-justify: inter-word;
+
+    h1 {
+      text-align: left;
+    }
+  }
+
+  .rating-dir {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .score {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    background: #fff;
+    color: #000;
+    font-weight: 850;
+    margin: 0;
+    border-radius: 50%;
+  }
+
+  .dir {
+    margin: 0 0 0 50px;
+
+    p {
+      margin: 0;
+    }
+
+    h1 {
+      @media screen and (max-width: 768px) {
+        font-size: var(--fontlg);
+      }
+    }
+  }
+
+  .run-time {
+    margin: 0 0 0 50px;
+
+  }
+`;
